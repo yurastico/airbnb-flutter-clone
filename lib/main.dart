cc60f819b0b8bb551d5c26a6ca3aff236b137aed
yurastico <cunha.ysc@gmail.com>
2024-02-05 19:43:57 -0300

    creating navigationBar (TabBar in swiftUI)

--- lib/main.dart
+++ lib/main.dart
@@ -21,8 +21,48 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  int currentPageIndex = 0;
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Container());
+    return Scaffold(
+        bottomNavigationBar: NavigationBar(
+          onDestinationSelected: (int index) {
+            setState(() {
+              currentPageIndex = index;
+            });
+          },
+          selectedIndex: currentPageIndex,
+          destinations: const <Widget>[
+            NavigationDestination(
+              icon: Icon(
+                Icons.today,
+              ),
+              label: 'Hoje',
+            ),
+            NavigationDestination(
+              icon: Icon(Icons.calendar_month),
+              label: 'Calendário',
+            ),
+            NavigationDestination(
+              icon: Icon(Icons.home_work_outlined),
+              label: 'Anúncios',
+            ),
+            NavigationDestination(
+              icon: Icon(Icons.messenger_outline),
+              label: 'Mensagens',
+            ),
+            NavigationDestination(
+              icon: Icon(Icons.menu),
+              label: 'Menu',
+            ),
+          ],
+        ),
+        body: [
+        const Center(child: Text("Hoje"),),
+        const Center(child: Text("Calendário"),),
+        const Center(child: Text("Anúncio"),),
+        const Center(child: Text("Mensagens"),),
+        const Center(child: Text("Menu"),),
+][currentPageIndex]);
   }
 }
