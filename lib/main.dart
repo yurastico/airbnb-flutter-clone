249f8a9be98ee8ac19a1f26e81b2c3435f188987
yurastico <cunha.ysc@gmail.com>
2024-02-05 21:24:07 -0300

    changing theme and appbar

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:airbnb_fiap_2/utils.dart';
+import 'package:airbnbistic/utils.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -6,16 +6,21 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        appBarTheme: const AppBarTheme(
+          iconTheme: IconThemeData(color: Colors.black),
+          color: Colors.white,
+        ),
+      ),
       home: MyHomePage(),
     );
   }
-
 }
 
 class MyHomePage extends StatefulWidget {
@@ -26,7 +31,8 @@ class MyHomePage extends StatefulWidget {
     "Chegando em breve (1)",
     "Programados (1)",
     "Análise pendente (1)",
-    "Hóspedes no momento (1)"];
+    "Hóspedes no momento (1)"
+  ];
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
@@ -44,7 +50,7 @@ class _MyHomePageState extends State<MyHomePage> {
           elevation: 0,
           actions: [
             IconButton(
-                onPressed: (){},
+                onPressed: () {},
                 icon: const Icon(Icons.notifications_none_outlined))
           ],
         ),
@@ -57,7 +63,9 @@ class _MyHomePageState extends State<MyHomePage> {
           selectedIndex: currentPageIndex,
           destinations: const <Widget>[
             NavigationDestination(
-              icon: Icon(Icons.today,),
+              icon: Icon(
+                Icons.today,
+              ),
               label: 'Hoje',
             ),
             NavigationDestination(
@@ -84,97 +92,88 @@ class _MyHomePageState extends State<MyHomePage> {
           advertisingTab,
           messagesTab,
           menuTab,
-        ][currentPageIndex]
-    );
+        ][currentPageIndex]);
   }
 
-  get todayTab => Container(child:
-    Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      mainAxisSize: MainAxisSize.max,
-      children: [
-        insidePadding(
-            const Text("É um prazer ter você aqui de novo, Ricardo"),
-            bottomPadding: 64
-        ),
-        insidePadding(
-            const Text("Suas reservas")
-        ),
-        Container(
-          margin: const EdgeInsets.only(bottom: 24.0),
-          height: 70,
-          child: ListView(
-            scrollDirection: Axis.horizontal,
-            children: List.generate(5, (index) => buildChoice(index)),
+  get todayTab => Container(
+          child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisSize: MainAxisSize.max,
+        children: [
+          insidePadding(
+              const Text("É um prazer ter você aqui de novo, Ricardo"),
+              bottomPadding: 64),
+          insidePadding(const Text("Suas reservas")),
+          Container(
+            margin: const EdgeInsets.only(bottom: 24.0),
+            height: 70,
+            child: ListView(
+              scrollDirection: Axis.horizontal,
+              children: List.generate(5, (index) => buildChoice(index)),
+            ),
           ),
-        ),
-        insidePadding(
-            Container(
-              margin: const EdgeInsets.only(bottom: 48.0),
-              width: double.maxFinite,
-              child: Column(
-                children: [
-                  Padding(
-                    padding: EdgeInsets.all(16.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Padding(
-                          padding: EdgeInsets.only(bottom: 48.0),
-                          child: Text("Checkout amanhã"),
-                        ),
-                        Row(
-                          children: [
-                            Expanded(
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Text("Michael"),
-                                  Text("25 - 26 de jan."),
-                                ],
-                              ),
-                            ),
-                            CircleAvatar(
-                              backgroundImage: AssetImage("images/download.jpeg"),
-                              radius: 24,
-                            )
-                          ],
-                        )
-                      ],
-                    ),
-                  ),
-                  Row(
+          insidePadding(Container(
+            margin: const EdgeInsets.only(bottom: 48.0),
+            width: double.maxFinite,
+            child: Column(
+              children: [
+                const Padding(
+                  padding: EdgeInsets.all(16.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      Expanded(
-                        child: TextButton(
-                          onPressed: (){},
-                          child: Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Text("Enviar mensagem"),
-                          ),
-                        ),
+                      Padding(
+                        padding: EdgeInsets.only(bottom: 48.0),
+                        child: Text("Checkout amanhã"),
                       ),
-                      Expanded(
-                        child: TextButton(
-                          onPressed: (){},
-                          child: Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Text("Ligar"),
+                      Row(
+                        children: [
+                          Expanded(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text("Michael"),
+                                Text("25 - 26 de jan."),
+                              ],
+                            ),
                           ),
-                        ),
-                      ),
+                          CircleAvatar(
+                            backgroundImage: AssetImage("images/download.jpeg"),
+                            radius: 24,
+                          )
+                        ],
+                      )
                     ],
                   ),
-                ],
-              ),
-            )
-        ),
-        insidePadding(
-            const Text("Todas as reservas")
-        ),
-      ],
-    )
-  );
+                ),
+                Row(
+                  children: [
+                    Expanded(
+                      child: TextButton(
+                        onPressed: () {},
+                        child: const Padding(
+                          padding: EdgeInsets.all(8.0),
+                          child: Text("Enviar mensagem"),
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child: TextButton(
+                        onPressed: () {},
+                        child: const Padding(
+                          padding: EdgeInsets.all(8.0),
+                          child: Text("Ligar"),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          )),
+          insidePadding(const Text("Todas as reservas")),
+        ],
+      ));
 
   get calendarTab => const Center(child: Text("Calendário"));
   get advertisingTab => const Center(child: Text("Anúncio"));
@@ -182,17 +181,16 @@ class _MyHomePageState extends State<MyHomePage> {
   get menuTab => const Center(child: Text("Menu"));
 
   Widget buildChoice(int index) => Container(
-    margin: const EdgeInsets.only(left: 10),
-    child: ActionChip(
-      label: Text(
-        widget.textsOfChips[index],
-      ),
-      onPressed: () {
-        setState(() {
-          statesOfChips[index] = !statesOfChips[index];
-        });
-      },
-    ),
-  );
-
+        margin: const EdgeInsets.only(left: 10),
+        child: ActionChip(
+          label: Text(
+            widget.textsOfChips[index],
+          ),
+          onPressed: () {
+            setState(() {
+              statesOfChips[index] = !statesOfChips[index];
+            });
+          },
+        ),
+      );
 }
